a36a6b767b175b0dc4bd9007481b350497cda80e
Bret Hogg <hoggb@wwu.edu>
2017-11-01 19:04:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, pos)
-  if board[pos] == " " || board[pos] == ""
+  if board[pos] == " " || board[pos] == "" || board[pos] == nil
     false
   else
     true
